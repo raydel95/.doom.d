87d90f0ebafed0c5e6f0c70b28764971d817ad4d
Raydel Alonso <raydelalonsobaryolo@gmail.com>
2020-06-05 00:27:38 -0400

    add flyspell-lazy

--- config.el
+++ config.el
@@ -76,7 +76,7 @@
 (setq doom-theme 'doom-vibrant)                   ; theme
 (delq! t custom-theme-load-path)
 
-;; ;; (after! flyspell (require 'flyspell-lazy) (flyspell-lazy-mode 1)) ; use flyspell-lazy
+(after! flyspell (require 'flyspell-lazy) (flyspell-lazy-mode 1)) ; use flyspell-lazy
 ;; (after! company ;;company improvment
 ;;   (setq company-idle-delay 0.4
 ;;         company-minimum-prefix-length 2)
@@ -95,6 +95,7 @@
 ;;               company-files
 ;;               company-yasnippet))
 ;; (set-company-backend! 'ess-r-mode '(company-R-args company-R-objects company-dabbrev-code :separate))
+
 
 ;; (use-package! info-colors
 ;;   :commands (info-colors-fontify-node))
