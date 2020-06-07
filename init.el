df4743ec55a362634ba26c9d9e0cc82267550b7d
Raydel Alonso <raydelalonsobaryolo@gmail.com>
2020-06-07 19:37:09 -0400

    add ranger to dired

--- init.el
+++ init.el
@@ -69,7 +69,9 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +icons)             ; making dired pretty [functional]
+       (dired
+        +ranger
+        +icons)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        (ibuffer +icons)         ; interactive buffer management
        (undo +tree)              ; persistent, smarter undo for your inevitable mistakes
