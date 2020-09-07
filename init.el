fa6b5d0e1372bdcb986be8a76afa8a672494bd8e
Ivan Galban <ivan.galban.smith@gmail.com>
2020-09-07 19:39:07 -0400

    Add grammar and spell

--- init.el
+++ init.el
@@ -85,8 +85,8 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       ;; spell             ; tasing you for misspelling mispelling
-       ;; grammar           ; tasing grammar mistake every you make
+       (spell +flyspell)   ; tasing you for misspelling mispelling
+       grammar           ; tasing grammar mistake every you make
 
        :tools
        ansible
